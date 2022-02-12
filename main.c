c0ec4212b177944cd785d810a4e03e8e3500b969
jeff5197 <69752534+jeff5197@users.noreply.github.com>
2022-02-12 10:53:41 +0000

    Added jakes game and will probably be final edition

--- main.c
+++ main.c
@@ -1,17 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
-void pressTo()
-{
-	char input;
-	printf("Press enter to continue...");
-	do
-	{
-		fflush(stdin);#include"lingo.h"
+#include"lingo.h"
 #include"Mine Sweeper.h"
 #include"Casino.h"
 #include"chinesememory.h"
+#include"virusGame.h"
 
 #include"randomgame.h"
 
@@ -221,6 +212,7 @@ int main()
 
 					case 3:
 					//Jake's Virus Game, if ever finished
+					playVirus(&acctiveAccount);
 					writeAccount(&acctiveAccount);
 					break;
 
@@ -883,7 +875,7 @@ void setting()
 				}
 				else
 				{
-					playBox();
+					playbox();
 				}
 			}
 			break;
@@ -929,91 +921,4 @@ void setting()
 	}
 	sprintf(command, "color %c%c", backColor, frontColor);
 	system(command);
-}
-
-		input = getch();
-	}while(input != 13);
-
-	printf("\n");
-	system("cls");
-}
-
-
-int a = 10;
-void BoxGame()
-{
-	int i = 0;	
-	int score = 0;
-	while (i == 0)
-	{
-		system("cls");
-		int x = rand()%a;
-		int g = 0;
-		printf("           +--------------+\n          /|             /|\n         / |            / |\n        *--+-----------*  |\n        |  |           |  |\n        |  |           |  |\n        |  |           |  |\n        |  +-----------+--+\n        | /            | /\n        |/             |/\n        *--------------*");
-		printf("\n\nWhat is the number in the box? = ");
-		scanf("%d",&g);
-		system("cls");
-		printf("           +--------------+\n          /|             /|\n         / |            / |\n        *--+-----------*  |\n        |  |           |  |\n        |  |     %d     |  |\n        |  |           |  |\n        |  +-----------+--+\n        | /            | /\n        |/             |/\n        *--------------*",x);
-		if (g == x)
-		{
-			printf("\n\nCorrect!\n");
-			score++;
-		}
-		else
-		{
-			printf("\n\nWrong!\n");
-			i++;
-		}
-		printf("\nScore = %d\n\n",score);
-		pressTo();
-	}
-	printf("\nGame Over!\n\n");
-	system("PAUSE");
-	system("cls");
-}
-
-int playBox()
-{
-	system("cls");
-	srand (time(NULL));
-	int select = 0;
-	printf("Welcome to Guess the Number!\nWould you like to :\n1. Play\n2. Settings\n3. Exit\n\n:");
-	scanf("%d",&select);
-	system("cls");
-	switch (select)
-	{
-		case 1:
-			BoxGame();
-			break;
-		case 2:
-			printf("Adjust difficulty : \n1. Easy\n2. Medium\n3. Hard\n4. Expert\n\n:");
-			scanf("%d",&select);
-			switch (select)
-			{
-				case 1:
-					printf("Easy selected.\n");
-					a = 10;
-					break;
-				case 2:
-					printf("Medium selected.\n");
-					a = 30;
-					break;
-				case 3:
-					printf("Hard selected.\n");
-					a = 50;
-					break;
-				case 4:
-					printf("Expert selected.\n");
-					a = 100;
-					break;
-			}
-			system("PAUSE");
-			playBox();
-		case 3:
-			return 0; 
-		default:
-			printf("Invalid Input!\n");
-	}
-	playBox();
-	return 0;
 }
